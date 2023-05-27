97d6d4c1f498d26dad724f6c017a8ed3c8b23abf
p-monika <131166450+p-monika@users.noreply.github.com>
2023-05-28 00:13:18 +0530

    Create multiply float.c

---
+++ multiply float.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+void main(){
+
+float num1, num2, prod=0;
+
+printf("Enter the 2 float point values for multiplication");
+scanf("%f %f", &num1, &num2);
+
+prod = num1 * num2;
+
+printf("The product is %3.2f", prod);
+
+}
+
+//output
+Enter the 2 float point values for multiplication2.2
+3.2
+The product is 7.04
